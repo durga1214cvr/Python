8f36c310a1474cb5a6079242d4ec67f05e816252
durga1214cvr <123532799+durga1214cvr@users.noreply.github.com>
2023-02-03 15:32:09 +0530

    Add files via upload

---
+++ Areatri.py
@@ -0,0 +1,9 @@
+# a=10;
+# b=257;
+# a=(1/2)*a*b;
+# print(a)
+b=input("enter base value ");
+h=input("enter height value ");
+area=(1/2)*float(b)*float(h);
+print(area);
+1
---
+++ acii.py
@@ -0,0 +1,3 @@
+ch=input("enter a character");
+print("Ascii value of a character is: ",ord(ch));
+print('character of the ascii given +1 would be',chr(ord(ch)+1));
